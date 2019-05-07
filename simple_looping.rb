a196505fe759c49140855d48dd3513d7622aebf3
fortunate-jest-6040 <pedro.loureiro7@hotmail.com>
2019-05-07 14:16:44 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -19,7 +19,9 @@ end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
-  number_of_times.times
+  number_of_times.times{
+    ph
+  }
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
